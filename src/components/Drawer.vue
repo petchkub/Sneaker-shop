fe57cb455f66342c9e2292b7da9e501e2deb762e
petchkub <119925016+petchkub@users.noreply.github.com>
2024-02-15 21:04:17 +0700

    first commit

---
+++ src/components/Drawer.vue
@@ -0,0 +1,87 @@
+<script setup>
+import axios from 'axios'
+import { ref, computed, inject } from 'vue'
+import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
+import InfoBlock from './InfoBlock.vue'
+
+const props = defineProps({
+  totalPrice: Number,
+  vatPrice: Number
+})
+
+const { cart, closeDrawer } = inject('cart')
+
+const isCreating = ref(false)
+const orderId = ref(null)
+
+const createOrder = async () => {
+  try {
+    isCreating.value = true
+
+    const { data } = await axios.post(`https://64d1dad9fd506a70.mokky.dev/orders`, {
+      items: cart.value,
+      totalPrice: props.totalPrice.value
+    })
+
+    cart.value = []
+
+    orderId.value = data.id
+  } catch (err) {
+    console.log(err)
+  } finally {
+    isCreating.value = false
+  }
+}
+
+const cartIsEmpty = computed(() => cart.value.length === 0)
+const buttonDisabled = computed(() => isCreating.value || cartIsEmpty.value)
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
+
+    <div v-if="!totalPrice || orderId" class="flex h-full items-center">
+      <InfoBlock
+        v-if="!totalPrice && !orderId"
+        title="Cart is empty"
+        description="Add at least one pair of sneakers to complete your order."
+        image-url="/package-icon.png"
+      />
+      <InfoBlock
+        v-if="orderId"
+        title="Order is processed!"
+        :description="`Your order #${orderId} will be handed over to courier delivery soon`"
+        image-url="/order-success-icon.png"
+      />
+    </div>
+
+    <div v-else>
+      <CartItemList />
+
+      <div class="flex flex-col gap-4 mt-7">
+        <div class="flex gap-2">
+          <span>Total:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} $</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span>Tax 5%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} $</b>
+        </div>
+
+        <button
+          :disabled="buttonDisabled"
+          @click="createOrder"
+          class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+        >
+        Checkouts
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
